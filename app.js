e78a6ce6c1ee0f9e1619628d9b544a75d91e8f32
GraceMwende <gracemwendem768@gmail.com>
2020-09-14 18:51:29 +0300

    alert on add,removeand not fill all the books

--- app.js
+++ app.js
@@ -49,7 +49,16 @@ class UI {
     }
   }
 
-  static showAlert;
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+    // vanish in 3 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 2000);
+  }
   static clearFields() {
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
@@ -70,13 +79,16 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
 
   // validate
   if (title === "" || author === "" || isbn === "") {
-    alert("Please fill in all fields");
+    UI.showAlert("Please fill in all fields", "danger");
   } else {
     //instantiate book
     const book = new Book(title, author, isbn);
 
     //Add Book to UI
     UI.addBookToList(book);
+
+    //shoe sucess msg
+    UI.showAlert("Book Added", "success");
 
     // clear fields
     UI.clearFields();
@@ -86,4 +98,7 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
 //Event:Remove a Book
 document.querySelector("#book-list").addEventListener("click", (e) => {
   UI.deleteBook(e.target);
+
+  //show delete message
+  UI.showAlert("Book Removed", "success");
 });
